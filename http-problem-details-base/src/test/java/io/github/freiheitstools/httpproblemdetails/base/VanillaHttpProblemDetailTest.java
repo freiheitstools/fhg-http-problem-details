5e46db758e17a3c8ba58606599cb67f513fa5497
Oliver B. Fischer <oliver.fischer@freiheitsgrade.consulting>
2024-01-09 11:06:27 +0100

    Pitest will not fail on empty modules
    Some, currently one one module, does not have any code. The default
    behavior of Pitest is to fail, if no mutations have been found.
    
    Pitest was reconfigured not to fail if there are no mutations.
    This might only happen in this project if there is no code
    at all in a module.

--- http-problem-details-base/src/test/java/io/github/freiheitstools/httpproblemdetails/base/VanillaHttpProblemDetailTest.java
+++ http-problem-details-base/src/test/java/io/github/freiheitstools/httpproblemdetails/base/VanillaHttpProblemDetailTest.java
@@ -2,6 +2,7 @@ package io.github.freiheitstools.httpproblemdetails.base;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
